6fff96426fc22507d4c910b5488b32ffbe5f1605
Curtis <curtis99877@gmail.com>
2016-09-09 20:37:12 -0400

    link subcomponents to app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header'
+import { FooterComponent } from './footer'
+import { GameComponent } from './game'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    HeaderComponent,
+    FooterComponent,
+    GameComponent,
+  ]
 })
 export class AppComponent {
   title = 'app works!';
