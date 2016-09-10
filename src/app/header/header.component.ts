ff687a7e317ce2c77e5c96a577de682a19f4e32b
Curtis <curtis99877@gmail.com>
2016-09-09 21:51:31 -0400

    whitespace

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+
   title = "REFUGEE CHOICE";
-  
+
   constructor() {
   }
 
