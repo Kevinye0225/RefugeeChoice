a706348929907c05e9d8d8a5a51c1caaea452bd9
Curtis <curtis99877@gmail.com>
2016-09-10 22:32:17 -0400

    css

--- src/app/game/questions.service.ts
+++ src/app/game/questions.service.ts
@@ -9,21 +9,21 @@ export class QuestionsService {
   constructor() { 
   	let a: Choice = {
   		id: 1,
-  		description: "How are you?",
+  		description: "You’ve finally reached the Mongolian border, but the guards stop you and threaten to deport you and your mother back to China. What do you do?",
       options: [
         <Option>{
           id: 1,
-          description: "Option 1",
+          description: "Take out your knife and pledge to kill yourself on the spot",
           choiceId: 2
         },
         <Option>{
           id: 2,
-          description: "Option 2",
+          description: "Follow the guards to avoid confrontation and think of a way to escape later",
           choiceId: 2
         },
         <Option>{
           id: 3,
-          description: "Option 3",
+          description: "Bribe the guards with all the money you have, knowing that you’ll be going on with empty pockets",
           choiceId: 2
         },
       ],
